59233bbdcf462059113dfd8b5abb2f086a0b4e7a
anjalie.cooray <anjalie.cooray_nh@ntucguest.com>
2024-07-05 15:59:24 +0530

    Fusion auth react integration

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import {
+  FusionAuthProvider,
+  FusionAuthProviderConfig,
+} from "@fusionauth/react-sdk";
+
+const config: FusionAuthProviderConfig = {
+  clientId: "db721c24-edd3-4167-a41d-9d086985676d",
+  redirectUri: "http://localhost:3000",
+  postLogoutRedirectUri: "http://localhost:3000/logged-out",
+  serverUrl: "http://localhost:9011",
+  shouldAutoFetchUserInfo: true,
+  shouldAutoRefresh: true,
+  onRedirect: (state?: string) => {
+    console.log(`Redirect happened with state value: ${state}`);
+  },
+  scope: 'openid email profile offline_access'
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <FusionAuthProvider {...config}>
+        <App />
+      </FusionAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
